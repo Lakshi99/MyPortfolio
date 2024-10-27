f93a7807fc2ed8a2d924a9495b2c683f4b58e251
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-10-28 00:55:07 +0530

    changed some css

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">Portfolio</a>
-      <div className={styles.menu}>
+      <div className={styles.menu}>  
+      <img className={styles.menuBtn} src="/src/assets/nav/menuIcon.png" alt="menu icon" />
         <ul className={styles.menuItems}>
             <li>
             <a href="/">About Me</a>
