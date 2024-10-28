d42fd202e0c15786c77e48a2fecedff0bdd243c8
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-10-28 23:45:36 +0530

    changed some css

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,18 @@ import React, {useState} from 'react'
 import styles from "./Navbar.module.css";
 import { getImageurl } from '../../utils';
 
-const Navbar = () => {
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">Portfolio</a>
       <div className={styles.menu}>  
-      <img className={styles.menuBtn} src="/src/assets/nav/menuIcon.png" alt="menu icon" />
+      <img className={styles.menuBtn} src={menuOpen
+      ? ("/src/assets/nav/closeIcon.png")
+      : ("/src/assets/nav/menuIcon.png")
+      
+      }
+       alt="menu icon" />
         <ul className={styles.menuItems}>
             <li>
             <a href="/">About Me</a>
