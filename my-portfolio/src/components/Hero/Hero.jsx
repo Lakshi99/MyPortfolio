62ada9529b8d3bee1140676890adb5a3b82ef327
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-10-30 21:53:53 +0530

    Hera page css

--- my-portfolio/src/components/Hero/Hero.jsx
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import HeroImage from '../../assets/about/Hero.jpeg';
+import styles from './Hero.module.css';
 
 
 const Hero = () => {
-  return <section>
-    <div>
-        <h1>Hi! I'm Lakshika</h1>
-        <h2>Full Stack Developer</h2>
-        <p>I’m a Full Stack Developer and a Software Engineering undergraduate who loves building cool web apps and bringing ideas to life! With a blend of frontend and backend skills, I'm all about creating user-friendly, impactful digital experiences. Let's create something amazing together! </p>
-        <a href = "mailto:lakshikabandara601@gmail.com">Contact Me</a>
+  return <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi! I'm <span className={styles.highlightName}>Lakshika</span></h1>
+        <h2 className={styles.title1}>Full Stack Developer</h2>
+        <p className={styles.description}>I’m a Full Stack Developer and a Software Engineering undergraduate who loves building cool web apps and bringing ideas to life! With a blend of frontend and backend skills, I'm all about creating user-friendly, impactful digital experiences. Let's create something amazing together! </p>
+        <a href = "mailto:lakshikabandara601@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
     
-    <img src={HeroImage} alt="Hero" className="hero-image" />
-    <div></div>
+    <img src={HeroImage} alt="Hero" className={styles.heroImg} />
+    <div className={styles.topblur} />
+    <div className={styles.bottomblur} />
 
   </section>
     
