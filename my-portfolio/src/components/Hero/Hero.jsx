7358da309ec8f1bb066ee27a354b22d33192b450
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-10-31 01:27:52 +0530

    develop the hero section

--- my-portfolio/src/components/Hero/Hero.jsx
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeroImage from '../../assets/about/Hero.jpeg';
+import HeroImage from '../../assets/about/Hero.jpg';
 import styles from './Hero.module.css';
 
 
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className={styles.content}>
         <h1 className={styles.title}>Hi! I'm <span className={styles.highlightName}>Lakshika</span></h1>
         <h2 className={styles.title1}>Full Stack Developer</h2>
-        <p className={styles.description}>I’m a Full Stack Developer and a Software Engineering undergraduate who loves building cool web apps and bringing ideas to life! With a blend of frontend and backend skills, I'm all about creating user-friendly, impactful digital experiences. Let's create something amazing together! </p>
+        <p className={styles.description}>I’m a Full Stack Developer who loves building cool web apps and bringing ideas to life! With a blend of frontend and backend skills, I'm all about creating user-friendly, impactful digital experiences. Let's create something amazing together! </p>
         <a href = "mailto:lakshikabandara601@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
     
